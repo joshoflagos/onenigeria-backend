0b797ab3d5a528462a1e0b8c5ab3b1b83b26d9f5
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-04-07 02:08:04 +0000

    feat(auth): integrate whoami authentication with Prisma repositories and global guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import {
+  WhoamiAuthGuard,
+} from '@odysseon/whoami-adapter-nestjs';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
-    })
+    }),
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: WhoamiAuthGuard,
+    },
+  ],
 })
 export class AppModule { }
