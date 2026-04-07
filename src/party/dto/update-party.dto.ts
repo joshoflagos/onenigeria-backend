b8ae5ce95ae103be88c82b2837cdd162c7476c26
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-04-07 00:31:28 +0000

    chore: clean up

--- src/party/dto/update-party.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePartyDto } from './create-party.dto';
-
-export class UpdatePartyDto extends PartialType(CreatePartyDto) {}
--- src/user/dto/update-profile.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { OnboardingDto } from './onboarding.dto';
-
-export class UpdateProfileDto extends PartialType(OnboardingDto) {}
